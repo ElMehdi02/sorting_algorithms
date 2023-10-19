5cbc69738fd03fb8f3f4689c1ba30eb08ac3586d
ElMehdi02 <bensoudamehdi6@gmail.com>
2023-10-19 10:25:22 -0700

    sorting_algorithms

---
+++ sort.h
@@ -0,0 +1,33 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+int *make_counting(int max, int size, int *array);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
+#endif /* SORT_H */

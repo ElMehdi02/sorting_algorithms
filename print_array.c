8b910abdd78f38b22f468f8e10b6b317a35858d5
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-10-22 20:18:48 +0100

    Update print_array.c

--- print_array.c
+++ print_array.c
@@ -1,4 +1,5 @@
-#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
@@ -8,15 +9,15 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
